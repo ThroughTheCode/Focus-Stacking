1e839b3c7c236951a4b67d4c47c823901fcea232
ThroughTheCode <39703642+ThroughTheCode@users.noreply.github.com>
2018-05-30 21:32:16 +0200

    Update ILP.m

--- ILP.m
+++ ILP.m
@@ -1,13 +1,13 @@
 function [IP]= ILP(Pyr, a)
-[~, Level]=size(Pyr.G);
-
-for i=Level-1:-1:1
-    clc;
-    disp(['Collapse pyramid at level : ', num2str(i)]);
-    Pyr.G{i}=Expand(Pyr.G{i+1}, a)+Pyr.L{i};
-    Pyr.G{i+1}={};
-end
-IP=Pyr;
+% This function does the inverse construction of the laplacian pyramid Pyr with the parameter a used for the gaussian kernel.
+    [~, LvlMax]=size(Pyr.G);
+    for i=LvlMax-1:-1:1
+        clc;
+        disp(['Collapse pyramid at level : ', num2str(i)]);
+        Pyr.G{i}=Expand(Pyr.G{i+1}, a)+Pyr.L{i};
+        Pyr.G{i+1}={};
+    end
+    IP=Pyr;
 end
 
 
