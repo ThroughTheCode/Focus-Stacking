4cc2d76388ee4ea0ed7e36c8659abdac88355ba5
ThroughTheCode <39703642+ThroughTheCode@users.noreply.github.com>
2018-05-30 21:24:56 +0200

    Update FOCUS_STACKING.m

--- FOCUS_STACKING.m
+++ FOCUS_STACKING.m
@@ -1,31 +1,33 @@
-clc; close all; clear;
+clc; close all; clear all;
 tic
 %% Loading Images
 [~, IBW, H, W, nbframe]=LoadingImg('Demo_from_Helicon_Focus/', 'DSC0', 8953, 8956, '.jpg', 1);
 
 %% Generating in-focus maps
-sigma=0.5;
-threshold_for_quantile=0.8; % 0<t<1
-se_size=10;
+sigma=0.5; %parameter used for computing the gaussian gradient 
+threshold_for_quantile=0.8; % parameter used for the threshold of the gaussian gradient
+se_size=10; % parameter used for dilating the threshold
 InfocusMaps = In_focus_Maps(IBW, sigma, threshold_for_quantile, se_size);
 
-%% Generating binary maps and gray leveled pyramids
+%% Generating in-focus maps and gray leveled pyramids
 a=0.5;
 InfocusGP=StackGP(InfocusMaps, a);
 IBWLP=StackLP(IBW, a);
 for i=1:size(InfocusGP.G, 2)
-    IBWLP.L{i}=IBWLP.L{i}.*InfocusGP.G{i};
+    IBWLP.L{i}=IBWLP.L{i}.*InfocusGP.G{i}; %Pre-selection of the in-focus pixels
 end
-clear InfocusMaps;
+clearvars InfocusMaps InfocusGP;
 
-%% Fusing Pyramids
+%% Fusing the grayed pyramids
 FusedLP=PyramidFusion(IBWLP, a);
-clear IBW;
+clearvars IBW IBWLP;
 
 %% Generating all-in-focus BW image
 FusedLP=ILP(FusedLP, a);
 Iref=FusedLP.G{1}(1:H, 1:W);
 clear FusedLP;
+
+%% Showing the fused image
 figure;
 imshow(Iref);
 toc;
