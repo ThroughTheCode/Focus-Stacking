412a400df5c5575f67e584d602a440bf28ebbf80
ThroughTheCode <39703642+ThroughTheCode@users.noreply.github.com>
2018-05-31 23:34:34 +0200

    Update Kernel.m

--- Kernel.m
+++ Kernel.m
@@ -1,5 +1,5 @@
 function [w2d] = Kernel(a)
-w1d=[1/4-a/2; 1/4 ;a; 1/4; 1/4-a/2];
-w2d=w1d*w1d';
+% This function computes a kernel used for expanding and reducing images of the pyramid with the parameter a.
+  w1d=[1/4-a/2; 1/4 ;a; 1/4; 1/4-a/2];
+  w2d=w1d*w1d';
 end
-
