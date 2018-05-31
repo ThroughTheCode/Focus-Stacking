0a01a5ec3d7f1ea87f84e2809a605e9742428c7a
ThroughTheCode <39703642+ThroughTheCode@users.noreply.github.com>
2018-05-31 23:43:40 +0200

    Update Pyramid.m

--- Pyramid.m
+++ Pyramid.m
@@ -1,10 +1,11 @@
 function [ PyrDef ] = Pyramid(Level, type)
-switch(type)
-    case 'Laplacian'
-        PyrDef=struct('G', {cell(1, Level)}, 'L', {cell(1, Level)});
-    case 'Gaussian'
-        PyrDef=struct('G', {cell(1, Level)});
-    otherwise
-        error(['The type ', type, ' is unknown']);
-end
+% This function gives a definition of the data contained in the pyramid according its type and height (level).
+    switch(type)
+        case 'Laplacian'
+            PyrDef=struct('G', {cell(1, Level)}, 'L', {cell(1, Level)});
+        case 'Gaussian'
+            PyrDef=struct('G', {cell(1, Level)});
+        otherwise
+            error(['The type ', type, ' is unknown']);
+    end
 end
