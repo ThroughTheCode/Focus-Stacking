d6ce12eef0a04bf24fc8ee0d7423cd557d094137
ThroughTheCode <39703642+ThroughTheCode@users.noreply.github.com>
2018-06-02 15:32:32 +0200

    Update PyramidFusion.m

--- PyramidFusion.m
+++ PyramidFusion.m
@@ -1,9 +1,13 @@
 function [FusPyr] = PyramidFusion(Pyrs, a)
-[~, Level]=size(Pyrs.G);
-FusPyr=Pyramid(Level, 'Laplacian');
+% This function computes the fusion of the pyramids at each level according to the local region energy for all the levels excepting
+% the top for which we use a criteria of entropy and standard deviation.
 
+[~, LvlMax]=size(Pyrs.G);
+FusPyr=Pyramid(Level, 'Laplacian'); %Retrieve the type of the pyramid
+
+%% Applying the criteria of maximal local region energy
 w=Kernel(a);
-for lvl=1:Level-1
+for lvl=1:LvlMax-1
     clc;
     disp(['Fusing pyramids at lvl ', num2str(lvl)]);
     LocalRegionEnergy=abs(convn(Pyrs.L{lvl}.^2, w, 'same'));
@@ -15,6 +19,7 @@ for lvl=1:Level-1
 end
 clear LocalRegionEnergy; 
 
+%% Applying the criteria of maximal entropy and standard deviation
 [~, idxE]=max((entropyfilt(Pyrs.G{end})), [], 3);
 [~, idxD]=max((stdfilt(Pyrs.G{end})), [], 3);
 [r, c, ~]=size(Pyrs.G{end});
@@ -25,4 +30,3 @@ idx  = [1:N]+(idxD(1:N)-1)*N;
 GendD = reshape(Pyrs.G{end}(idx), r, c);
 FusPyr.G{end}=(GendE+GendD)/2;
 end
-
